c842a04660172c932101e65d6a1c125797cc6238
exe-rios <eriosdeveloper@gmail.com>
2026-09-18 11:56:29 -0300

    resolver problemas de bugs con electron

--- frontend/electron/preload.ts
+++ frontend/electron/preload.ts
@@ -1,6 +1,21 @@
-import { contextBridge } from 'electron';
+import { contextBridge, ipcRenderer } from 'electron';
+
+export interface MessageBoxOptions {
+  type?: 'none' | 'info' | 'error' | 'question' | 'warning';
+  title?: string;
+  message: string;
+  buttons?: string[];
+  defaultId?: number;
+  cancelId?: number;
+}
 
 /** Expone de forma segura variables del entorno de Electron a la ventana de renderizado. */
 contextBridge.exposeInMainWorld('electronAPI', {
   platform: process.platform,
+  showMessageBoxSync: (options: MessageBoxOptions): number => {
+    return ipcRenderer.sendSync('dialog:showMessageBoxSync', options);
+  },
+  refocus: (): void => {
+    ipcRenderer.send('window:refocus');
+  }
 });
